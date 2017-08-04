50051b33b5336ab4e6c45c517a5fb39d413d4cf1
bitbrian <bitbrian@bitbrian.com>
2017-08-04 21:02:32 +0900

    Add similiar exlude directory to cscope as ctags has

--- cscope_gen.sh
+++ cscope_gen.sh
@@ -2,12 +2,23 @@
 
 PWD=`pwd`
 
+# delete all old tags for my sanity
+find . -type f -name tags -exec rm {} \;
+find . -type f -name "cscope.*" -exec rm {} \;
+
 find ${PWD}/ -name '*.py' \
 -o -name '*.java' \
 -o -iname '*.[CH]' \
 -o -name '*.cpp' \
 -o -name '*.cc' \
 -o -name '*.hpp'  \
+-o -name .git -prune \
+-o -name .svn -prune \
+-o -name .hg -prune \
+-o -name bundle -prune \
+-o -name vendor -prune \
+-o -name min -prune \
+-o -name builds -prune \
 > cscope.files 2>/dev/null
 
 # -b: just build the cross-reference only
